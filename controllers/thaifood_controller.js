5584459f15cd1290e608185503496d4ba633b45d
Lawan Petza <Lawan.Petza@gmail.com>
2018-07-18 01:42:39 -0400

    modified js

--- controllers/thaifood_controller.js
+++ controllers/thaifood_controller.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var Food = require("../models/thaifood.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
 
+router.get("/", function (req, res) {
   Food.all(function (data) {
     var hbsObject = {
       thaiFood: data
@@ -17,45 +17,48 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/thaiFood", function (req, res) {
-  Food.create([
-    "thaiFoodName"],
-    [
-      req.body.thaiFoodName
-    ], function () {
+router.post("/", function (req, res) {
+  Food.create(req.body.thaiFoodName,function () {
       // Send back the ID of the new quote
       res.redirect("/");
     });
 });
 
-router.put("api/thaiFood/:id", function (req, res) {
+// Devour food
+// router.post('/Food/eat/:id', function (req, res) {
+//   burger.update(req.params.id, function() {
+//     res.redirect('/index');
+//   });
+// });
+
+router.put("/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   Food.update({
-    devoured: 0
-  }, condition, function (result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+    devoured: req.body.devoured
+  }, condition, function () {
+    // if (result.changedRows == 0) {
+    //   // If no rows were changed, then the ID must not exist, so 404
+    //   return res.status(404).end();
+    // } else {
+    //   res.status(200).end();
+    // }
     res.redirect("/");
   });
 });
 
-router.delete("api/thaiFood/:id", function (req, res) {
+router.delete("/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  Food.delete(condition, function (result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  Food.delete(condition, function () {
+    // if (result.affectedRows == 0) {
+    //   // If no rows were changed, then the ID must not exist, so 404
+    //   return res.status(404).end();
+    // } else {
+    //   res.status(200).end();
+    // }
     res.redirect("/");
   });
 });
